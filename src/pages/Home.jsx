ae6e06747e6714f383b4723251d0729dc415ef6c
saba <sabamansuri147@gmail.com>
2023-06-02 17:57:00 +0530

    used useQuery on Home page to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,26 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
 
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  const [filter, setFilter] = useState(null);
+
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () =>
+      filter.searchOptions === 'shows'
+        ? searchForShows(filter.q)
+        : searchForPeople(filter.q),
+   
+    enabled: !!filter,
+    refetchOnWindowFocus : false,
+  });
 
   const onSearch = async ({ q, searchOptions }) => {
-    try {
-      setApiDataError(null);
-
-      let result;
-      if (searchOptions === 'shows') {
-        result = await searchForShows(q);
-      } else {
-        result = await searchForPeople(q);
-      }
-      setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
+    setFilter({ q, searchOptions });
   };
 
   const renderApiData = () => {
